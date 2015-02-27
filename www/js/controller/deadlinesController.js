79200f365f4620254123b1b64cd01939708552d8
zirkler <zirkler@users.noreply.github.com>
2015-02-27 17:28:26 +0100

    group lectures by there date

--- www/js/controller/deadlinesController.js
+++ www/js/controller/deadlinesController.js
@@ -21,6 +21,11 @@ angular.module('wuw.controllers')
 
 .controller('DeadlinesDetailCtrl', function($scope, $stateParams, Deadlines) {
     $scope.deadline = Deadlines.get($stateParams.deadlineId);
+    $scope.markAsDoneText = "Mark as Done (only for you)";
+    
+    $scope.saveLocally = function() {
+        console.log("save");
+    }
 })
 
 .controller('DeadlinesCreateCtrl', function($scope, $state, Deadlines, $timeout, Settings, $http) {
