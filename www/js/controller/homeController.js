4ef1ead543c9d64e5a6413b3618fd227de63a418
TheJetlag <TheJetlag@users.noreply.github.com>
2015-03-15 15:19:09 +0100

    display current and next lecture on the home tab

--- www/js/controller/homeController.js
+++ www/js/controller/homeController.js
@@ -1,4 +1,6 @@
 angular.module('wuw.controllers')
 
-.controller('HomeCtrl', function($scope) {
+.controller('HomeCtrl', function($scope, Lectures) {
+  $scope.currLecture = Lectures.getCurrentLecture();
+  $scope.nextLecture = Lectures.getNextLecture();
 })
