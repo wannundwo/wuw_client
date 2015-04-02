5028dceed50b667fe1a4e00618950eef1f99e786
zirkler <zirkler@users.noreply.github.com>
2015-04-02 11:19:27 +0200

    remove ionic comments

--- www/js/app.js
+++ www/js/app.js
@@ -1,12 +1,5 @@
 "use strict";
 
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
 angular.module('wuw', ['ionic', 'wuw.controllers', 'wuw.services', 'wuw.filters', 'angular.filter'])
 
 .run(function($ionicPlatform, Settings) {
@@ -36,8 +29,6 @@ angular.module('wuw', ['ionic', 'wuw.controllers', 'wuw.services', 'wuw.filters'
     }
   });
 })
-
-
 
 .config(function($stateProvider, $urlRouterProvider) {
 
