1e90849916865e695bf78689601171fe1e9b5bd5
zirkler <zirkler@users.noreply.github.com>
2015-04-28 22:48:29 +0200

    v0.3.0

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@ angular.module('wuw', ['ionic', 'wuw.controllers', 'wuw.services', 'wuw.directiv
 .run(function($ionicPlatform, $state, $rootScope, Settings) {
 
     var apiUrl = "https://wuw.benleb.de:4342/api/v0";
-    var versionNumber = "0.2.0";
+    var versionNumber = "0.3.0";
 
     // predefined settings
     Settings.setSetting('version', versionNumber);
