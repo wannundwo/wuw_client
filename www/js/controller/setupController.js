cf9f68888af74d81db62a4fd0ec6f7da5e5d1dad
TheJetlag <TheJetlag@users.noreply.github.com>
2015-04-30 19:27:49 +0200

    setup: make setup more robust

--- www/js/controller/setupController.js
+++ www/js/controller/setupController.js
@@ -20,32 +20,35 @@ angular.module('wuw.controllers')
     };
 
     $scope.saveSetup = function() {
-        // receive all groups where something is checked
-        // TODO: Some ngModel magic could make this easier.
-        var selectedGroups = [];
-        var selectedLectures = [];
-        for (var i = 0; i < $scope.groups.length; i++) {
-            var newGroup = true;
-            for (var j = 0; j < $scope.groups[i].lectures.length; j++) {
-                if ($scope.groups[i].lectures[j].chosen) {
-                    if (newGroup) {
-                        selectedGroups.push($scope.groups[i]._id);
-                        newGroup = false;
+        // receive all chosen groups and lectures
+        try {
+            var selectedGroups = [];
+            var selectedLectures = [];
+            for (var i = 0; i < $scope.groups.length; i++) {
+                var newGroup = true;
+                for (var j = 0; j < $scope.groups[i].lectures.length; j++) {
+                    if ($scope.groups[i].lectures[j].chosen) {
+                        if (newGroup) { // make sure a group is added only once
+                            selectedGroups.push($scope.groups[i]._id);
+                            newGroup = false;
+                        }
+                        selectedLectures.push({
+                            lectureName: $scope.groups[i].lectures[j].lectureName,
+                            groupName: $scope.groups[i]._id
+                        });
                     }
-                    selectedLectures.push({
-                        lectureName: $scope.groups[i].lectures[j].lectureName,
-                        groupName: $scope.groups[i]._id
-                    });
                 }
             }
+
+            Settings.setSetting("selectedGroups", JSON.stringify(selectedGroups));
+            Settings.setSetting("selectedLectures", JSON.stringify(selectedLectures));
+            Settings.setSetting("localDeadlines", "");
         }
-
-        Settings.setSetting("selectedGroups", JSON.stringify(selectedGroups));
-        Settings.setSetting("selectedLectures", JSON.stringify(selectedLectures));
-        Settings.setSetting("localDeadlines", "");
-        $state.go("tab.home", {location: "replace"});
+        catch(e) { }
+        finally {
+            // in any case, goto the home tab
+            $state.go("tab.home", {location: "replace"});
+        }
     };
-
     $scope.load();
-
 });
