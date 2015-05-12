9ceb6154289a166cce7a3105a3ce96d3dc86c14f
zirkler <zirkler@users.noreply.github.com>
2015-05-12 09:53:46 +0200

    lectures: clean up controllers

--- www/js/controller/lecturesListController.js
+++ www/js/controller/lecturesListController.js
@@ -3,34 +3,6 @@
 angular.module("wuw.controllers")
 
 .controller("LecturesListCtrl", function($scope, $state, $ionicHistory, $ionicPopup, $timeout, $filter, Lectures, Settings) {
-    
-    var date = new Date();
-    var d = date.getDate();
-    var m = date.getMonth();
-    var y = date.getFullYear();
-
-    /* config object */
-    $scope.uiConfig = {
-      calendar:{
-        height: "auto",
-        editable: false,
-        //defaultView: "agendaWeek",
-        defaultView: "month",
-        header: false,
-        dayClick: $scope.alertEventOnClick,
-        eventDrop: $scope.alertOnDrop,
-        eventResize: $scope.alertOnResize
-      }
-    };
-    
-    $scope.events = [
-      {title: 'Long Event', start: new Date(y, m, d - 5),end: new Date(y, m, d - 2)},
-      {id: 999,title: 'Repeating Event',start: new Date(y, m, d - 3, 16, 0),allDay: false},
-      {id: 999,title: 'Repeating Event',start: new Date(y, m, d + 4, 16, 0),allDay: false},
-      {title: 'Birthday Party',start: new Date(y, m, d + 1, 19, 0),end: new Date(y, m, d + 1, 22, 30),allDay: false},
-    ];
-    
-    $scope.eventSource = [$scope.events];
 
     $scope.loadLectures = function() {
         Lectures.lecturesForGroups().then(function(lectures){
