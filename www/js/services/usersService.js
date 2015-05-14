05fae2188eb19931e01d4d7e9809e7e0228322c6
Ben Lebherz <git@benleb.de>
2015-05-14 13:56:17 +0200

    user: ping back home

---
+++ www/js/services/usersService.js
@@ -0,0 +1,33 @@
+"use strict";
+
+angular.module('wuw.services')
+
+.factory('Users', function($http, $q, Settings) {
+
+    // add a new deadline (to server and locally)
+    var ping = function() {
+
+        var deferred = $q.defer();
+        $http({
+            url: Settings.getSetting("apiUrl") + '/users',
+            method: "POST",
+            headers: {'Content-Type': 'application/json'},
+            data: {
+                deviceId: device.uuid,
+                platform: ionic.Platform.platform(),
+                platformVersion: ionic.Platform.version()
+            }
+        })
+        .then(function(response) {
+            deferred.resolve(response);
+        },
+        function(response) {
+            deferred.reject(response);
+        });
+        return deferred.promise;
+    };
+
+    return {
+        ping: ping,
+    };
+});
