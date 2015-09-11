0a225276f242c56f834db5f8301dcbdd4bb30c45
zirkler <zirkler@users.noreply.github.com>
2015-09-11 03:42:42 +0200

    implemented news and events

---
+++ www/js/services/newsService.js
@@ -0,0 +1,24 @@
+/*jshint bitwise: false*/
+
+"use strict";
+
+angular.module("wuw.services")
+
+.factory("News", function($http, $q, Settings) {
+    var getNews = function() {
+        var deferred = $q.defer();
+
+        $http.get(Settings.getSetting("apiUrl") + "/news")
+        .success(function(data, status, headers, config) {
+            deferred.resolve(data);
+        })
+        .error(function(data, status, headers, config) {
+            deferred.reject(data);
+        });
+        return deferred.promise;
+    };
+
+    return {
+        getNews: getNews
+    };
+});
