e5b4f6677d6783ba433b26511de5ab320a9983ae
TheJetlag <chris@Chriss-MacBook-Pro.local>
2015-11-01 11:19:08 +0100

    events + news: show a initial loading spinner

--- www/js/controller/eventsController.js
+++ www/js/controller/eventsController.js
@@ -4,8 +4,16 @@ angular.module('wuw.controllers')
 
 .controller('EventsCtrl', function($scope, Events) {
     $scope.loadEvents = function() {
+        if (!$scope.events) {
+            $scope.initialLoading = true;
+        }
+
         Events.getEvents().then(function(events){
             $scope.events = events;
+        }, function(){
+        }).finally(function(){
+            $scope.initialLoading = false;
+            $scope.$broadcast("scroll.refreshComplete");
         });
     }
 
--- www/js/controller/newsController.js
+++ www/js/controller/newsController.js
@@ -4,8 +4,16 @@ angular.module('wuw.controllers')
 
 .controller('NewsCtrl', function($scope, News) {
     $scope.loadNews = function() {
+        if (!$scope.news) {
+            $scope.initialLoading = true;
+        }
+
         News.getNews().then(function(news){
             $scope.news = news;
+        }, function(){
+        }).finally(function(){
+            $scope.initialLoading = false;
+            $scope.$broadcast("scroll.refreshComplete");
         });
     }
 
