0bca4d26367ef47bee8ddb8d4c9cd183fcabb3b2
zirkler <zirkler@users.noreply.github.com>
2015-11-19 11:12:47 +0100

    v0.5.4

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@ angular.module('wuw', ['ionic', 'wuw.controllers', 'wuw.services', 'wuw.directiv
 .run(function($ionicPlatform, $state, $rootScope, Settings, Users) {
 
     var apiUrl = "https://app.hft-stuttgart.de:4342/api/v0";
-    var versionNumber = "0.5.2";
+    var versionNumber = "0.5.4";
 
     // predefined settings
     Settings.setSetting('version', versionNumber);
