1a7fcfce624e28c80a14a58ce3584aeaf60facd0
Ben Lebherz <git@benleb.de>
2016-04-10 10:51:04 +0200

    release: 0.6.5

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@ angular.module('wuw', ['ionic', 'wuw.controllers', 'wuw.services', 'wuw.directiv
 .run(function($ionicPlatform, $state, $rootScope, Settings, Users) {
 
     var apiUrl = "https://app.hft-stuttgart.de:4342/api/v0";
-    var versionNumber = "0.6.1";
+    var versionNumber = "0.6.5";
 
     // predefined settings
     Settings.setSetting('version', versionNumber);
