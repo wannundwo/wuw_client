1f601ebf601827bc17524e72173497cacf7f137c
zirkler <zirkler@users.noreply.github.com>
2016-05-08 17:45:33 +1000

    calendar view: make dummy data timezone independent

--- www/js/controller/lecturesWeeklyController.js
+++ www/js/controller/lecturesWeeklyController.js
@@ -9,71 +9,71 @@ angular.module("wuw.controllers")
         [
             {
                 title: "M1",
-                startTime: moment("2016-05-02T08:20:00+1000", moment.ISO_8601).toDate(),
-                endTime: moment("2016-05-02T09:00:00+1000", moment.ISO_8601).toDate()
+                startTime: moment.utc("2016-05-02T08:20:00+1000", moment.ISO_8601).toDate(),
+                endTime: moment.utc("2016-05-02T09:00:00+1000", moment.ISO_8601).toDate()
             },
             {
                 title: "M1",
-                startTime: moment("2016-05-02T10:00:00+1000", moment.ISO_8601).toDate(),
-                endTime: moment("2016-05-02T11:00:00+1000", moment.ISO_8601).toDate()
+                startTime: moment.utc("2016-05-02T10:00:00+1000", moment.ISO_8601).toDate(),
+                endTime: moment.utc("2016-05-02T11:00:00+1000", moment.ISO_8601).toDate()
             }
         ],
         [
             {
                 title: "A1",
-                startTime: moment("2016-05-03T08:30:00+1000", moment.ISO_8601).toDate(),
-                endTime: moment("2016-05-03T11:30:00+1000", moment.ISO_8601).toDate()
+                startTime: moment.utc("2016-05-03T08:30:00+1000", moment.ISO_8601).toDate(),
+                endTime: moment.utc("2016-05-03T11:30:00+1000", moment.ISO_8601).toDate()
             },
             {
                 title: "A1",
-                startTime: moment("2016-05-03T12:00:00+1000", moment.ISO_8601).toDate(),
-                endTime: moment("2016-05-03T15:30:00+1000", moment.ISO_8601).toDate()
+                startTime: moment.utc("2016-05-03T12:00:00+1000", moment.ISO_8601).toDate(),
+                endTime: moment.utc("2016-05-03T15:30:00+1000", moment.ISO_8601).toDate()
             },
             {
                 title: "A2",
-                startTime: moment("2016-05-03T12:30:00+1000", moment.ISO_8601).toDate(),
-                endTime: moment("2016-05-03T14:00:00+1000", moment.ISO_8601).toDate()
+                startTime: moment.utc("2016-05-03T12:30:00+1000", moment.ISO_8601).toDate(),
+                endTime: moment.utc("2016-05-03T14:00:00+1000", moment.ISO_8601).toDate()
             },
             {
                 title: "A3",
-                startTime: moment("2016-05-03T13:30:00+1000", moment.ISO_8601).toDate(),
-                endTime: moment("2016-05-03T18:30:00+1000", moment.ISO_8601).toDate()
+                startTime: moment.utc("2016-05-03T13:30:00+1000", moment.ISO_8601).toDate(),
+                endTime: moment.utc("2016-05-03T18:30:00+1000", moment.ISO_8601).toDate()
             }
         ],
         [
             {
                 title: "B1",
-                startTime: moment("2016-05-04T14:30:00+1000", moment.ISO_8601).toDate(),
-                endTime: moment("2016-05-04T16:00:00+1000", moment.ISO_8601).toDate()
+                startTime: moment.utc("2016-05-04T14:30:00+1000", moment.ISO_8601).toDate(),
+                endTime: moment.utc("2016-05-04T16:00:00+1000", moment.ISO_8601).toDate()
             },
             {
                 title: "B2",
-                startTime: moment("2016-05-04T16:00:00+1000", moment.ISO_8601).toDate(),
-                endTime: moment("2016-05-04T17:00:00+1000", moment.ISO_8601).toDate()
+                startTime: moment.utc("2016-05-04T16:00:00+1000", moment.ISO_8601).toDate(),
+                endTime: moment.utc("2016-05-04T17:00:00+1000", moment.ISO_8601).toDate()
             }
         ],
         [
             {
                 title: "DO1",
-                startTime: moment("2016-05-05T10:00:00+1000", moment.ISO_8601).toDate(),
-                endTime: moment("2016-05-05T11:00:00+1000", moment.ISO_8601).toDate()
+                startTime: moment.utc("2016-05-05T10:00:00+1000", moment.ISO_8601).toDate(),
+                endTime: moment.utc("2016-05-05T11:00:00+1000", moment.ISO_8601).toDate()
             }
         ],
         [
             {
                 title: "F1",
-                startTime: moment("2016-05-06T10:00:00+1000", moment.ISO_8601).toDate(),
-                endTime: moment("2016-05-06T11:00:00+1000", moment.ISO_8601).toDate()
+                startTime: moment.utc("2016-05-06T10:00:00+1000", moment.ISO_8601).toDate(),
+                endTime: moment.utc("2016-05-06T11:00:00+1000", moment.ISO_8601).toDate()
             },
             {
                 title: "A1",
-                startTime: moment("2016-05-06T12:00:00+1000", moment.ISO_8601).toDate(),
-                endTime: moment("2016-05-06T13:10:00+1000", moment.ISO_8601).toDate()
+                startTime: moment.utc("2016-05-06T12:00:00+1000", moment.ISO_8601).toDate(),
+                endTime: moment.utc("2016-05-06T13:10:00+1000", moment.ISO_8601).toDate()
             },
             {
                 title: "A2",
-                startTime: moment("2016-05-06T12:30:00+1000", moment.ISO_8601).toDate(),
-                endTime: moment("2016-05-06T14:00:00+1000", moment.ISO_8601).toDate()
+                startTime: moment.utc("2016-05-06T12:30:00+1000", moment.ISO_8601).toDate(),
+                endTime: moment.utc("2016-05-06T14:00:00+1000", moment.ISO_8601).toDate()
             }
         ]
     ];
