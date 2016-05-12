a5066e50cc7a995ad012efff9d2aa83583db06d3
zirkler <zirkler@users.noreply.github.com>
2016-05-12 14:47:50 +1000

    week view: draw borders around event blocks

--- www/lib/czWeekView/czWeekView.js
+++ www/lib/czWeekView/czWeekView.js
@@ -176,6 +176,7 @@ angular.module('wuw.czWeekView', [])
                     eventDiv.className += ' weekViewEvent';
                     eventDiv.style.height = eventMinutes * pixelPerMinute + 'px';
                     eventDiv.style.background = event.color;
+                    eventDiv.style.border = "1px solid " + shadeColor(event.color, -0.2);
 
                     // place text inside the eventContentDiv
                     var eventContentDiv = document.createElement('div');
@@ -355,6 +356,14 @@ angular.module('wuw.czWeekView', [])
         var day = d.getDay();
         var diff = d.getDate() - day + (day == 0 ? -6:1); // adjust when day is sunday
         return new Date(d.setDate(diff));
+    }
+
+    /*
+     * Darkens a given hex color.
+     */
+    function shadeColor(color, percent) {   
+        var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
+        return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
     }
 
     /*
