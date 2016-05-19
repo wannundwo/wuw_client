4591644d474bb8cd61ee729bfac82c61225245ee
zirkler <zirkler@users.noreply.github.com>
2016-05-19 17:21:11 +1000

    week view: show popovers on click

--- www/js/controller/lecturesWeeklyController.js
+++ www/js/controller/lecturesWeeklyController.js
@@ -4,30 +4,49 @@ angular.module('wuw.controllers')
 
 .controller('LecturesWeeklyCtrl', function($scope, $locale, $ionicHistory, $translate, $ionicPopover, $state, $ionicPopup, Lectures, Settings) {
     
+    var lectures = [];
     $scope.events = [[],[],[]];
 
-    $scope.onEventClick = function(eventId){
-        console.log("lecturesWeeklyCtrl", eventId);
+    $ionicPopover.fromTemplateUrl('templates/lecturePopover.html', {
+        scope: $scope
+    }).then(function(popover) {
+        $scope.popover = popover;
+    });
+
+
+    $scope.onEventClick = function(data){
+        // find lecture by event id
+        var eventId = data.eventId;
+        var clickEvent = data.clickEvent;
+        var lectures = Lectures.fromCache();
+        for (var i = 0; i < lectures.length; i++) {
+            var lecture = lectures[i];
+            if (lecture.id == eventId) {
+                $scope.clickedLecture = lecture;
+                break;
+            }
+        }
+        
+        $scope.popover.show(clickEvent);
+    };
+
+    $scope.closePopover = function() {
+        $scope.popover.hide();
     };
 
     $scope.loadLectures = function() {
         Lectures.lecturesForUser().then(function(lectures){
-            $scope.lectures = lectures;
+            lectures = lectures;
+            var grouped = groupLecturesForWeek(lectures);
+            assignEvents(grouped);
             $scope.$broadcast('czErrorMessage.hide'); //hide an eventually shown error message
         }, function(error) {
-            if (error === 'httpFailed') {
-                // show the error message with some delay to prevent flickering
-                $timeout(function() {
-                    $scope.$broadcast('czErrorMessage.show');
-                }, 300);
-            }
+            $timeout(function() {
+                $scope.$broadcast('czErrorMessage.show');
+            }, 300);
         }).finally(function () {
            $scope.$broadcast('scroll.refreshComplete');
         });
-    };
-
-    $scope.doRefresh = function() {
-        $scope.loadLectures();
     };
 
     $scope.$on('$ionicView.afterEnter', function(){
@@ -39,7 +58,6 @@ angular.module('wuw.controllers')
     function assignEvents(groupedLectures) {
         var groupedArray = [];
         for (var weekIdentifier in groupedLectures) {
-            
             groupedArray.push(groupedLectures[weekIdentifier]);
         }
         $scope.eventsWeekly = groupedArray;
@@ -54,7 +72,7 @@ angular.module('wuw.controllers')
             var lecture = lectures[i];
             lecture.startTime = moment(lecture.startTime, moment.ISO_8601);
             lecture.endTime = moment(lecture.endTime, moment.ISO_8601);
-            lecture.title = lecture.lectureName;
+            lecture.title = lecture.lectureName + '<br>' + lecture.rooms.join(', ');
             
             if (lecture.startTime.isoWeekday() > 5) {
                 hasWeekendLectures = true;
@@ -95,6 +113,7 @@ angular.module('wuw.controllers')
     };
 
     $scope.$on("$ionicView.beforeEnter", function(event, data) {
+        // eventuall redirect user to its preferred lecture view
         if (Settings.getSetting('lecturesView') === 'lecturesList') {
             $ionicHistory.nextViewOptions({ disableAnimate: true, disableBack: true });
             $state.go('tab.lecturesWeekly', {location: 'replace'});      
